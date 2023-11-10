ae513340946964269310935c42203c4a1cc33976
Nicolas Schonfeld <nicolas.schonfeld@25watts.com.ar>
2023-11-10 20:57:03 -0300

    v1.0.1

--- src/components/CardsComponent/CardsComponent.jsx
+++ src/components/CardsComponent/CardsComponent.jsx
@@ -6,6 +6,11 @@ import DesignIcon from "../Icons/DesignIcon";
 import IconsIcon from "../Icons/IconsIcon";
 import DatabaseIcon from "../Icons/DatabaseIcon";
 import Link from "next/link";
+import StylesIcon from "../Icons/StylesIcon";
+import AnimationIcon from "../Icons/AnimationIcon";
+import ChartIcon from "../Icons/ChartIcon";
+import ApiIcon from "../Icons/ApiIcon";
+import PlusIcon from "../Icons/PlusIcon";
 
 const CardsComponent = () => {
   const cards = [
@@ -25,16 +30,42 @@ const CardsComponent = () => {
       icon: <DesignIcon w="130" />,
     },
     {
-      title: "Design",
-      path: "/design",
+      title: "Animation",
+      path: "/animation",
+      icon: <AnimationIcon w="130" />,
+    },
+    {
+      title: "Styles",
+      path: "/styles",
+      icon: <StylesIcon w="130" />,
+    },
+    {
+      title: "DataBase",
+      path: "/db",
       icon: <DatabaseIcon w="130" />,
+    },
+
+    {
+      title: "Chrats",
+      path: "/charts",
+      icon: <ChartIcon w="130" />,
+    },
+    {
+      title: "Api",
+      path: "/apis",
+      icon: <ApiIcon w="130" />,
+    },
+    {
+      title: "More",
+      path: "/more",
+      icon: <PlusIcon w="130" />,
     },
   ];
 
   return (
     <AnimatePresence>
-      <section className="w-full h-screen">
-        <div className="relative w-full h-full flex items-center justify-start flex-col container mx-auto pt-40 pb-20 px-20 gap-10">
+      <section className="w-full h-full">
+        <div className="relative w-full h-auto flex items-center justify-start flex-col container mx-auto pt-10 pb-20 px-20 gap-10">
           <motion.div
             initial={{ scaleX: 0 }}
             animate={{ scaleX: 1 }}
@@ -44,29 +75,28 @@ const CardsComponent = () => {
           <motion.div
             initial={{ opacity: 0, h: 0 }}
             animate={{ opacity: 1, h: 1 }}
-            transition={{ duration: 0.5, delay: 0.4 }}
+            transition={{ duration: 0.5, delay: 0.7 }}
             className="w-full h-full bg-gradient-to-b from-white/10 absolute top-0 left-0"
           ></motion.div>
 
-          <div className="overflow-hidden w-full absolute top-10">
-            <motion.div
-              initial={{ opacity: 0, y: 100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.5 }}
-              className="w-full h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 py-10 px-20"
-            >
-              {cards?.map((card, index) => (
-                <Link href={card?.path} key={index}>
-                  <motion.div
-                    whileHover={{ scale: 1.02 }}
-                    className="w-full h-[409px] cursor-pointer bg-[#1A1A1A] border-2 border-[#555] hover:border-[#ffffff90] rounded-3xl flex items-center justify-center transition-all"
-                  >
-                    {card?.icon}
-                  </motion.div>
-                </Link>
-              ))}
-            </motion.div>
-          </div>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            className="w-full h-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 py-10 px-20"
+          >
+            {cards?.map((card, index) => (
+              <Link href={card?.path} key={index}>
+                <motion.div
+                  whileHover={{ scale: 1.02 }}
+                  className="relative w-full overflow-hidden h-96 cursor-pointer bg-[#1A1A1A] border-2 border-[#555] hover:border-[#ffffff90] rounded-3xl flex items-center justify-center transition-all"
+                >
+                  <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[#000000] transition-all"></div>
+                  {card?.icon}
+                </motion.div>
+              </Link>
+            ))}
+          </motion.div>
         </div>
       </section>
     </AnimatePresence>
---
+++ src/components/Icons/StylesIcon.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const StylesIcon = ({ w = "188", h = "188" }) => {
+  return (
+    <svg
+      width={w}
+      height={h}
+      viewBox="0 0 188 188"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M67.1429 0H120.857V188H67.1429V0ZM0 188V67.1429H53.7143V188H0ZM26.8571 0C19.7342 0 12.903 2.82958 7.86627 7.86627C2.82958 12.903 0 19.7342 0 26.8571C0 33.9801 2.82958 40.8113 7.86627 45.848C12.903 50.8847 19.7342 53.7143 26.8571 53.7143C33.9801 53.7143 40.8113 50.8847 45.848 45.848C50.8847 40.8113 53.7143 33.9801 53.7143 26.8571C53.7143 19.7342 50.8847 12.903 45.848 7.86627C40.8113 2.82958 33.9801 0 26.8571 0ZM134.286 0H188V53.7143H134.286V0ZM188 67.1429H134.286V120.857H188V67.1429Z"
+        fill="url(#paint0_linear_1_37)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1_37"
+          x1="94"
+          y1="0"
+          x2="94"
+          y2="188"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#D8D8D8" />
+          <stop offset="1" stop-color="#727272" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default StylesIcon;
